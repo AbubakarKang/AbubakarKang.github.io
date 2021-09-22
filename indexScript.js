9bfc1f01ebd142dcde151af11e3a87a38da210a9
AbubakarKang <90056797+AbubakarKang@users.noreply.github.com>
2021-09-22 19:40:04 +0800

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -5,7 +5,10 @@ const phoneThemeIcon = document.querySelector('[data-phoneThemeIcon]')
 const phoneMenuOpen = document.querySelector('[data-phoneMenuOpen]')
 const phoneMenuClose = document.querySelector('[data-phoneMenuClose]')
 const phoneNav = document.querySelector('[data-phoneNav]')
-
+const discordMedia = document.querySelector('[data-discordMedia]')
+const aboutContentButton = document.querySelector('[data-switchAboutContent]')
+const leftAboutContent = document.querySelector('[data-leftContent]')
+const rightAboutContent = document.querySelector('[data-rightContent]')
 
 //------------------------------------- FUNCTION CALLS -------------------------------------\\
 
@@ -13,6 +16,8 @@ themeIcon.addEventListener("click", toggleDisplayTheme)
 phoneMenuOpen.addEventListener("click", openPhoneMenu)
 phoneMenuClose.addEventListener("click", closePhoneMenu)
 phoneThemeIcon.addEventListener("click", toggleDisplayTheme)
+discordMedia.addEventListener("click", clipboardDiscord)
+aboutContentButton.addEventListener("click", changeAboutContent)
 
 //----------------------------------------- OTHERS -----------------------------------------\\
 
@@ -33,10 +38,26 @@ function toggleDisplayTheme() {
     }
 }
 
+function changeAboutContent() {
+    if (rightAboutContent.style.display == "none") {
+        rightAboutContent.style.display = "block"
+        leftAboutContent.style.display = "none"
+        aboutContentButton.innerHTML = `Designing`
+    } else {
+        rightAboutContent.style.display = "none"
+        leftAboutContent.style.display = "block"
+        aboutContentButton.innerHTML = `Coding`
+    }
+}
+
 function openPhoneMenu() {
     phoneNav.style.right = "0"
 }
 
 function closePhoneMenu() {
     phoneNav.style.right = "-250px"
+}
+
+function clipboardDiscord() {
+    navigator.clipboard.writeText("TheHacker#4418");
 }
