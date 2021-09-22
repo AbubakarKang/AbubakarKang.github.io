86b2bfccd6cf97255420be9dde55b794b5d04ede
AbubakarKang <90056797+AbubakarKang@users.noreply.github.com>
2021-09-22 13:14:38 +0800

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -1,22 +1,42 @@
 //---------------------------------------- IMPORTS ----------------------------------------\\
 
 const themeIcon = document.querySelector('[data-themeIcon]')
+const phoneThemeIcon = document.querySelector('[data-phoneThemeIcon]')
+const phoneMenuOpen = document.querySelector('[data-phoneMenuOpen]')
+const phoneMenuClose = document.querySelector('[data-phoneMenuClose]')
+const phoneNav = document.querySelector('[data-phoneNav]')
+
 
 //------------------------------------- FUNCTION CALLS -------------------------------------\\
 
 themeIcon.addEventListener("click", toggleDisplayTheme)
+phoneMenuOpen.addEventListener("click", openPhoneMenu)
+phoneMenuClose.addEventListener("click", closePhoneMenu)
+phoneThemeIcon.addEventListener("click", toggleDisplayTheme)
 
 //----------------------------------------- OTHERS -----------------------------------------\\
 
-
+phoneNav.style.right = "-250px"
 
 //---------------------------------------- FUNCTIONS ----------------------------------------\\
 
 function toggleDisplayTheme() {
     document.body.classList.toggle("darkMode")
     if (document.body.classList.contains("darkMode")) {
+        phoneThemeIcon.classList = "far fa-sun"
         themeIcon.classList = "far fa-sun"
+        phoneMenuOpen.src = "./images/webMenuDark.png"
     } else {
+        phoneThemeIcon.classList = "far fa-moon"
         themeIcon.classList = "far fa-moon"
+        phoneMenuOpen.src = "./images/webMenuLight.png"
     }
+}
+
+function openPhoneMenu() {
+    phoneNav.style.right = "0"
+}
+
+function closePhoneMenu() {
+    phoneNav.style.right = "-250px"
 }
